7144953ec2f68bad30151491a8884e0f70643d3c
Kardane <64183602+Kardane@users.noreply.github.com>
2026-09-20 16:12:12 +0900

    feat: wire robot damage and death events

--- src/main/java/dev/kardane/autobattle/event/AutoBattleEvents.java
+++ src/main/java/dev/kardane/autobattle/event/AutoBattleEvents.java
@@ -2,6 +2,7 @@ package dev.kardane.autobattle.event;
 
 import dev.kardane.autobattle.match.MatchManager;
 import dev.kardane.autobattle.tactics.PlanExecutor;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
@@ -17,6 +18,18 @@ public final class AutoBattleEvents {
             matchManager.tick(server);
             planExecutor.tick(matchManager.serverTick());
         });
+
+        ServerLivingEntityEvents.ALLOW_DAMAGE.register(
+            matchManager::allowDamage
+        );
+
+        ServerLivingEntityEvents.AFTER_DAMAGE.register(
+            matchManager::handleAfterDamage
+        );
+
+        ServerLivingEntityEvents.AFTER_DEATH.register(
+            matchManager::handleAfterDeath
+        );
 
         ServerLifecycleEvents.SERVER_STOPPED.register(
             server -> planExecutor.clear()
