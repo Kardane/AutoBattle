cce00d6b7d2613c4b2710920d4122c7f8b6e3304
Kardane <64183602+Kardane@users.noreply.github.com>
2026-09-22 11:29:25 +0900

    refactor: add shared plan validity policy

---
+++ src/main/java/dev/kardane/autobattle/tactics/PlanValidityStatus.java
@@ -0,0 +1,12 @@
+package dev.kardane.autobattle.tactics;
+
+public enum PlanValidityStatus {
+    VALID,
+    TARGET_MISSING,
+    TARGET_DEAD,
+    TARGET_INELIGIBLE,
+    WRONG_MATCH,
+    OUTSIDE_ARENA,
+    OUT_OF_RANGE,
+    TEMPORARILY_UNREACHABLE
+}
